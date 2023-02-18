c6e7f273c3216386dc2614b8eb3aa036a4f8c07c
anushkam <sri.anushka03@gmail.com>
2023-02-18 16:47:17 +0530

    Add code

--- 3.c
+++ 3.c
@@ -2,6 +2,19 @@
 #include <stdio.h>
 
 int main() {
-    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+    int arr[10] = {1,12,33,44,57,67,73,89,97,100};
     int n = sizeof(arr)/sizeof(int);
+    int x = 0;
+
+    printf("Enter the number you wish to search for: ");
+    scanf("%d", &x);    
+    int pos = bin_search(arr, 0, n-1, x);
+    printf("Element %d is at position %d.\n", x, pos+1);
+}
+
+int bin_search(int arr[], int low, int high, int x) {    
+    int mid = (low + high)/2;
+    if (arr[mid] == x) return mid;
+    else if (arr[mid] > x) bin_search(arr, low, mid - 1, x);        
+    else bin_search(arr, mid + 1, high, x);
 }
