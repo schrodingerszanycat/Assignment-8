07315bce59118d1d20ffea95da91db18ff951826
anushkam <sri.anushka03@gmail.com>
2023-02-19 12:16:49 +0530

    ADD

---
+++ assignment9/transpose.c
@@ -0,0 +1,26 @@
+
+
+#include <stdio.h>
+
+int main() {
+    int r = 0, c = 0;
+    printf("Enter the row and column size of the matrix: ");
+    scanf("%d %d", &r, &c);
+
+    
+}
+
+int swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int transpose(int arr, int r, int c) {
+    for (int i = 0; i<r; i++) {
+        for(int j = 0; j < i; j++) {
+            
+        }
+    }
+
+}
