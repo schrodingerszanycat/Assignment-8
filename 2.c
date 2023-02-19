f061f6e3efd1fbc92a037875b4523e384516acf5
anushkam <sri.anushka03@gmail.com>
2023-02-19 10:30:15 +0530

    Add code

--- 2.c
+++ 2.c
@@ -11,22 +11,20 @@ int main() {
     scanf("%d", &size);
     int arr[size];
     printf("Enter the elements of the array: ");
-    for (int i = 0; i<size; i++) scanf("%d", arr[i]);
+    for (int i = 0; i<size; i++) scanf("%d", &arr[i]);
     
     int x = 0;
     printf("Enter the element you wish to search for: ");
     scanf("%d", &x);
 
     int val = linear_search(arr, size, x);
-    if (val != -1) printf("Element %d is at index %d.", x, val);
-    else printf("Element not found.");
+    if (val == -1) printf("Element not found.");
+    else printf("Element %d is at position %d.\n", x, val+1);
 }
 
 int linear_search(int arr[], int size, int x) {
     for (int i = 0; i<size; i++) {
-        if (arr[i] == x){
-            return i;
-        }
-    return -1;
+        if (arr[i] == x) return i;
     }
+    return -1;
 }
