168a168c89f0294c124c6b354cff415f479e76ac
anushkam <sri.anushka03@gmail.com>
2023-02-19 15:29:02 +0530

    Add code

--- AssignmentIX/10.c
+++ AssignmentIX/10.c
@@ -1,7 +1,31 @@
 //10. Write a program in C to dynamically allocate and deallocate memory for a lower triangular matrix.
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     
+    int size = 0;
+    printf("Enter size of the square matrix: ");
+    scanf("%d", &size);
+    int **arr = (int **) calloc(size, sizeof(int *));
+    int n = size*(size-1)*0.5 + size;
+    printf("Enter all the lower triangular elements "
+    "(all %d of them)\n", n);
+    for (int i = 0; i<size; i++) {
+        arr[i] = (int *) calloc(size, sizeof(int));
+        for (int j = 0; j <= i; j++) {
+            scanf("%d", &arr[i][j]);
+        } 
+    }
+
+    for (int i = 0; i<size; i++) {
+        for (int j = 0; j<size; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    free(arr);
+    arr = NULL;
 }
