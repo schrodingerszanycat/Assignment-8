e05d33415b80007918b9e09e25d35393c468f209
anushkam <sri.anushka03@gmail.com>
2023-02-19 10:03:33 +0530

    Add

--- 5.c
+++ 5.c
@@ -1,39 +1,43 @@
-/*5. Write a function in C to implement selection sort.*/
-#include <stdio.h>
-int swap(int x, int y);
-int selection_sort(int arr[], int size);
+// 5. Write a function in C to implement selection sort.
 
+#include <stdio.h>
+
+int swap(int *x, int *y);
+
+void selection_sort(int arr[], int size);
 
 int main() { 
     int size = 0;
-    int arr[size];
     printf("Enter size of the array: ");
     scanf("%d", &size);
+    int arr[size];
     printf("Enter elements of the array: ");
-    for (int i = 0; i<size; i++) scanf("%d", arr[i]);
+    for (int i = 0; i<size; i++) scanf("%d", &arr[i]);
 
-    selection_sort(arr, size);    
+    selection_sort(arr, size);
 }
+
 int swap(int *x, int *y) {
     int temp = *x;
     *x = *y;
     *y = temp;
 }
-int selection_sort(int arr[], int size) {
-    int min = 0;
+
+void selection_sort(int arr[], int size) {
+    
     for (int i = 0; i<size; i++) {
+        int min = i;
         for (int j = i+1; j<size; j++) {
             if (arr[j]<arr[min]) {
-                swap(&arr[j], &arr[min]);
-                min = arr[j];
-            } 
-            else continue;
+                min = j;
+            }
         }
-
+        swap(&arr[i], &arr[min]);
     }
+
     printf("The new array is:\n");
     for (int j = 0; j<size; j++) {        
         printf("%d ", arr[j]);
     }
+    printf("\n");
 }
-
